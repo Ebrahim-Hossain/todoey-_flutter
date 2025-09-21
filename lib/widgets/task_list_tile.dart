640f2cf5cceac291ce6d6ba00b92e2ffea7fe690
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-09-21 22:26:05 +0600

    checkbox on tap added

--- lib/widgets/task_list_tile.dart
+++ lib/widgets/task_list_tile.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskListTile extends StatelessWidget {
-  const TaskListTile({super.key,});
+class TaskListTile extends StatefulWidget {
+  const TaskListTile({super.key});
 
+  @override
+  State<TaskListTile> createState() => _TaskListTileState();
+}
+
+class _TaskListTileState extends State<TaskListTile> {
+  bool isCheck = false;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: EdgeInsets.only(top: 30),
-      title: Text(
-        'This is task.',
-        style: TextStyle(fontSize: 20),
+      title: Text('This is task.', style: TextStyle(
+        fontSize: 20,
+        decoration: isCheck ? TextDecoration.lineThrough  : null,)
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
+      trailing: CheckBoxes(
+        isChecked: isCheck,
+        changeCheckbox: (newValue) {
+          setState(() {
+            isCheck = newValue;
+          });
+
+        },
       ),
     );
   }
 }
+
+class CheckBoxes extends StatelessWidget {
+  const CheckBoxes({super.key,required this.isChecked, required this.changeCheckbox});
+  final bool? isChecked;
+  final Function changeCheckbox;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (newValue) {
+        changeCheckbox(newValue);
+        }
+    );
+  }
+}
+
